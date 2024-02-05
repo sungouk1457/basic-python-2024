b2ae7c0ead95ec39905c792c2295020ae9eba6c9
sungouk1457 <sungouk1457@naver.com>
2024-02-05 16:58:23 +0900

    6일차 수업 정리

--- day06/test39_nothread.py
+++ day06/test39_nothread.py
@@ -1,5 +1,5 @@
-# file: test38_pyqt.py
-# desc: Qt디자이너로 만든 ui와 연동 
+# file: test39_nothread.py
+# desc: Qt에서 스레드 없이 동작테스트
 
 import sys
 from PyQt5 import QtGui,QtWidgets,uic
@@ -11,19 +11,21 @@ from PyQt5.QtWidgets import *
 class qtwin_exam(QWidget): 
     def __init__(self) -> None:
         super().__init__()
-        uic.loadUi('./day06/TestApp.ui',self)
+        uic.loadUi('./day06/ThreadApp.ui',self)
         # 버튼에 대한 시그널처리
-        self.btnstart.clicked.connect(self.btnstartClicked) #ui 파일내에 있는 위젯접근은 VScode상에서 색상으로 표시x
-        self.btnstop.clicked.connect(self.btnstopClicked)
-    
-    def btnstartClicked(self):
-        print('시작버튼 클릭')
-        self.lblstatus.setText('상태 : 동작시작')
-        QMessageBox.about(self,'동작','***시스템이 시작되었습니다')
+        self.btnStart.clicked.connect(self.btnStartClicked) #ui 파일내에 있는 위젯접근은 VScode상에서 색상으로 표시x
 
-    def btnstopClicked(self):
-        print('종료버튼 클릭')
-        self.lblstatus.setText('상태 : 동작중지')
+    
+    def btnStartClicked(self):
+        maxVal = 1000000
+        print('시작버튼 클릭')
+        self.pgbTask.setValue(0) #프로그레스바 0부터 시작
+        self.pgbTask.setRange(0,maxVal-1) # 0부터 100까지
+        for i in range(maxVal):
+            print_str = f'노쓰레드 출력 >> {i}'
+            print(print_str)
+            self.txbLog.append(print_str)
+            self.pgbTask.setValue(i)
 
     def closeEvent(self,QCloseEvent) -> None:
         re = QMessageBox.question(self,'종료확인','종료확인',QMessageBox.Yes|QMessageBox.No)
---
+++ day06/test40_thread.py
@@ -0,0 +1,71 @@
+# file: test39_nothread.py
+# desc: Qt\ 스레드 동작
+
+import sys
+from PyQt5 import QtGui,QtWidgets,uic
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class BackWorker(QThread): #PyQt에서 스레드 클래스 상속
+    initSignal = pyqtSignal(int) # 시그널을 UI스레드로 전달하기위한 변수객체
+    setSignal = pyqtSignal(int)
+    setLog = pyqtSignal(str)
+
+    def __init__(self, parent) -> None:
+        super().__init__(parent)
+        self.parent = parent #BackWorker에서 사용할 멤버변수
+
+    def run(self) -> None: # 스레드 실행
+        # 스레드로 동작할 내용
+        maxVal = 1000001
+        self.initSignal.emit(maxVal)
+        ###self.parent.pgbTask.setValue(0) #프로스레스바 0부터 시작  QThread에선 ui관련된 처리를 할 수 없음
+        ###self.parent.pgbTask.setRange(0,maxVal-1)
+        for i in range(maxVal):
+            print_str = f'쓰레드 출력 >> {i}'
+            print(print_str)
+            self.setSignal.emit(i)
+            self.setLog.emit(print_str)
+        ###    self.parent.txbLog.append(print_str)
+        ###    self.parent.pgbTask.setValue(i)
+    
+class qtwin_exam(QWidget): #UI스레드
+    def __init__(self) -> None:
+        super().__init__()
+        uic.loadUi('./day06/ThreadApp.ui',self)
+        self.btnStart.clicked.connect(self.btnStartClicked)
+        
+    def btnStartClicked(self):
+        th = BackWorker(self)
+        th.start()  #BackWorker 내의 self.run 실행
+        th.initSignal.connect(self.initpgbTask) # 스레드에서 초기화 시그널이 오면 initpgbTask 슬롯함수가 대신 처리
+        th.setSignal.connect(self.setpgbTask)
+        th.setLog.connect(self.settxbLog) #textbrowser 위젯에 진행사항 전달
+
+    def closeEvent(self,QCloseEvent) -> None:
+        re = QMessageBox.question(self,'종료확인','종료확인',QMessageBox.Yes|QMessageBox.No)
+        if re == QMessageBox.Yes:
+            QCloseEvent.accept()
+        else:
+            QCloseEvent.ignore()
+
+    #스레드에서 시그널이 넘어오면  UI처리를 대신 해주는 슬롯함수
+    @pyqtSlot(int) # BackWorker 스레드에서 self.initSignal.emit 동작해서 실행
+    def initpgbTask(self, maxVal):
+        self.pgbTask.setValue(0)
+        self.pgbTask.setRange(0,maxVal-1)
+
+    @pyqtSlot(str) # BackWorker 스레드에서 self.setLog.emit 동작해서 실행
+    def settxbLog(self,msg):
+        self.txbLog.append(msg)
+    
+    @pyqtSlot(int)
+    def setpgbTask(self, val):
+        self.pgbTask.setValue(val)
+
+if __name__ == '__main__':
+    loop = QApplication(sys.argv)
+    instance = qtwin_exam()
+    instance.show()
+    loop.exec_()    
